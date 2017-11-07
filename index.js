46b9bbc32018c7d58e1246be110ecc60161a4e3b
Antonia Ivanova <antonia.ivanova09@yahoo.com>
2017-11-07 15:33:30 +0100

    upload

---
+++ index.js
@@ -0,0 +1,27 @@
+//path from node
+const path = require('path');
+const electron = require('electron');
+const TimerTray = require('./app/timer_tray');
+const MainWindow = require('./app/main_window');
+
+const {app,ipcMain} = electron;
+
+let mainWindow;
+let tray;
+
+app.on('ready', ()=>{
+    app.dock.hide();
+    mainWindow = new MainWindow(`file://${__dirname}/src/index.html`);
+       
+    const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+
+    //takes different paths and joins them
+    const iconPath = path.join(__dirname,`./src/assets/${iconName}`);
+    
+    //variable pointing at the timer tray so it doesnt get garbage collected
+    tray = new TimerTray(iconPath, mainWindow);
+});
+
+ipcMain.on('update-timer', (event, timeLeft) => {
+   tray.setTitle(timeLeft); 
+});
